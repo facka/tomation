3da8aefe0d0cbfa3feaa642f71191f98817f6cf3
Facundo Crego <facundocrego@cloud.upwork.com>
2026-04-27 11:50:28 -0300

    Updated dependecies and fix config files

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [],
+      output: {
+        exports: 'named',
+      },
       plugins: [
         dts({
           pathsToAliases: false,
